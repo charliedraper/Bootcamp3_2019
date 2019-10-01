193b4bcbf3b57abe047926177f5518e269d7636a
Charles Draper <charlesdraper@ufl.edu>
2019-10-01 15:52:59 -0400

    Implemented listings server controller

--- Bootcamp3/server/controllers/listings.server.controller.js
+++ Bootcamp3/server/controllers/listings.server.controller.js
@@ -23,7 +23,6 @@ var mongoose = require('mongoose'),
 
 /* Create a listing */
 exports.create = function(req, res) {
-
   /* Instantiate a Listing */
   var listing = new Listing(req.body);
 
@@ -41,8 +40,9 @@ exports.create = function(req, res) {
       console.log(err);
       res.status(400).send(err);
     } else {
-      res.json(listing);
       console.log(listing)
+      res.json(listing);
+      
     }
   });
 };
@@ -58,24 +58,61 @@ exports.update = function(req, res) {
   var listing = req.listing;
 
   /* Replace the listings's properties with the new properties found in req.body */
- 
-  /*save the coordinates (located in req.results if there is an address property) */
- 
-  /* Save the listing */
+  listing.name = req.body.name;
+  listing.code = req.body.code;
+  listing.address = req.body.address;
 
+  /*save the coordinates (located in req.results if there is an address property) */
+  if (req.results) {
+    listing.coordinates = {
+      latitude: req.results.lat,
+      longitude: req.results.lng
+    };
+  }
+
+  /* Save the listing */
+  listing.save(function(err) {
+    if (err) {
+      console.log(err);
+      res.status(400).send(err);
+    }
+    else {
+      console.log(listing);
+      res.json(listing);
+      
+    }
+  });
 };
 
 /* Delete a listing */
 exports.delete = function(req, res) {
   var listing = req.listing;
 
-  /* Add your code to remove the listins */
-
+  /* Add your code to remove the listing */
+  Listing.findByIdAndDelete(listing._id, function(err) {
+    if (err) {
+      console.log(err);
+      res.status(400).send(err);
+    }
+    else {
+      console.log(listing);
+      res.json(listing);
+    }
+  })
 };
 
 /* Retreive all the directory listings, sorted alphabetically by listing code */
 exports.list = function(req, res) {
-  /* Add your code */
+  Listing.find({}, function(err, data) {
+    if (err) {
+      console.log(err);
+      res.status(400).send(err);
+    }
+    else {
+      res.send(data);
+    }
+  }).sort('code');
+  
 };
 
 /* 
@@ -88,6 +125,7 @@ exports.list = function(req, res) {
 exports.listingByID = function(req, res, next, id) {
   Listing.findById(id).exec(function(err, listing) {
     if(err) {
+      console.log(err);
       res.status(400).send(err);
     } else {
       req.listing = listing;
