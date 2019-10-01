28413642a20a39bbcc94232295c817b0a43668d7
Charles Draper <charlesdraper@ufl.edu>
2019-10-01 15:52:39 -0400

    Implemented listings model

--- Bootcamp3/server/models/listings.server.model.js
+++ Bootcamp3/server/models/listings.server.model.js
@@ -1,24 +1,33 @@
-//You can replace this entire file with your Bootcamp Assignment #2 - ListingSchema.js File
-
-/* Import mongoose and define any variables needed to create the schema */
 var mongoose = require('mongoose'), 
     Schema = mongoose.Schema;
 
-/* Create your schema */
+//Listing Schema
 var listingSchema = new Schema({
-  /* your code here from Bootcamp Assignment #2 - ListingSchema.js File*/
-    
+  code: {type: String, required: true},
+  name: {type: String, required: true},
+  coordinates: {
+    latitude: Number,
+    longitude: Number
+  },
+  address: String
 });
 
-/* create a 'pre' function that adds the updated_at and created_at if not already there property */
+//Before saving, do the following
 listingSchema.pre('save', function(next) {
-  /* your code here from Bootcamp Assignment #2 - ListingSchema.js File */
+  //Get the current date
+  var currentDate = new Date();
+
+  //Change the updated_at field to the current date
+  this.updated_at = currentDate;
+
+  //If created_at doesn't exist, add that to the field
+  if (!this.created_at) {
+    this.created_at = currentDate;
+  }
 
   next();
 });
 
-/* Use your schema to instantiate a Mongoose model */
+//Instantiate and export a mongoose model
 var Listing = mongoose.model('Listing', listingSchema);
-
-/* Export the model to make it avaiable to other parts of your Node application */
 module.exports = Listing;
